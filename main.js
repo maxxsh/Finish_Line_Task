5ae1ca9cf3c6f5e778b1c65bbd97426a47c28993
MAX SHESTOV <57421753+maxxsh@users.noreply.github.com>
2022-01-25 18:21:05 -0500

    Update main.js
    #Bugs Fixed:
    1. Show error message when nothing is found by the filter
    2. The handle functions were within the fetch function
    3. Remove the Next and Prev buttons when is no date
    #Enhanced the code
    1. Characters' names wrapped into h3 tags

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const initialUrl = "https://rickandmortyapi.com/api/character/";
 
+$(document).ready(function () {
+  $("#characterName").on("input", function () {
+    let urlName = initialUrl + "?name=" + $("#characterName").val();
+    $.startFunction(urlName);
+  });
+  $(".pageBtn").click(function () {
+    $.startFunction($(this).attr("data-card"));
+  });
+});
+
 $.startFunction = function (resourceUrl) {
+  console.log("vhod1");
   $.get(
     resourceUrl,
     function (data) {
@@ -8,40 +19,33 @@ $.startFunction = function (resourceUrl) {
         return (
           '<li><img src="' +
           card.image +
-          '"  alt="" /><div class="cardMeta">' +
+          '"  alt="" /><div class="cardMeta"><h3>' +
           card.name +
-          ' <button class="deleteBtn">Delete</button></div></li>'
+          '</h3><button class="deleteBtn">Delete</button></div></li>'
         );
       });
-      // add new elements to HTML DOM
       $("#cardList")[0].innerHTML = html.join("");
-      // delete bottons
+
+      if (data.info.next) {
+        $(".nextBtn").show();
+        $(".nextBtn").attr("data-card", data.info.next);
+      } else {
+        $(".nextBtn").hide();
+      }
+      if (data.info.prev) {
+        $(".prevBtn").show();
+        $(".prevBtn").attr("data-card", data.info.prev);
+      } else {
+        $(".prevBtn").hide();
+      }
       $(".deleteBtn").click(function () {
         $(this).parent().parent().slideUp();
       });
-      // page buttons
-      if (data.info.next == null) {
-        $(".prevBtn").hide();
-      } else {
-        $(".prevBtn").show();
-        $(".nextBtn").attr("data-card", data.info.next);
-      }
-      if (data.info.prev == null) {
-        $(".prevBtn").hide();
-      } else {
-        $(".prevBtn").show();
-        $(".prevBtn").attr("data-card", data.info.prev);
-      }
-      $(".pageBtn").click(function () {
-        $.startFunction($(this).attr("data-card"));
-      });
-      // filter
-      $("#characterName").on("input", function () {
-        let urlName = initialUrl + "?name=" + $("input").val();
-        $.startFunction(urlName);
-      });
     },
     "json"
-  );
+  ).fail(function () {
+    $("#cardList")[0].innerHTML = "<li>There is nothing here</li>";
+    $(".nextBtn").hide();
+  });
 };
 $.startFunction(initialUrl);
